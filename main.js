976d4423054aab978e7ed209a3df2d939bb7d77e
owenalexbell@gmail.com <>
2013-07-07 04:04:25 +0000

    First version of the code just to make sure that the pi calculation is working.

---
+++ main.js
@@ -0,0 +1,18 @@
+$(window).load(function() {
+  var places = 1000;
+
+  Big.DP = places;
+  Big.RM = 1;
+
+  var pi = new Big(0);
+  for (var i = 0; i < places; i++) {
+    var sum = new Big(16).pow(-1 * i);
+    var w = new Big(4/(8*i + 1));
+    var x = new Big(2/(8*i + 4));
+    var y = new Big(1/(8*i + 5));
+    var z = new Big(1/(8*i + 6));
+    pi = pi.plus(sum.times(w.minus(x).minus(y).minus(z)));
+    //pi.plus(sum);
+  }
+  alert(pi.toString());
+});
