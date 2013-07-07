7a8903e641ad565fd9346704f512d7f440da323c
Owen Bell <owenalexbell@gmail.com>
2013-07-07 18:36:06 -0400

    Basic version of web workers is now implemented.

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
-$(window).load(function() {
-  var places = 1000;
+//$(window).load(function() {
+var onLoad = function(e) {
+    /*
+    var places = 1000;
+    
+    Big.DP = places;
+    Big.RM = 1;
 
-  Big.DP = places;
-  Big.RM = 1;
-
-  var pi = new Big(0);
-  for (var i = 0; i < places; i++) {
-    var sum = new Big(16).pow(-1 * i);
-    var w = new Big(4/(8*i + 1));
-    var x = new Big(2/(8*i + 4));
-    var y = new Big(1/(8*i + 5));
-    var z = new Big(1/(8*i + 6));
-    pi = pi.plus(sum.times(w.minus(x).minus(y).minus(z)));
-    //pi.plus(sum);
-  }
-  alert(pi.toString());
-});
+    var time = new Date().getTime();
+    var pi = new Big(0);
+    for (var i = 0; i < places; i++) {
+        var sum = new Big(16).pow(-1 * i);
+        var w = new Big(4/(8*i + 1));
+        var x = new Big(2/(8*i + 4));
+        var y = new Big(1/(8*i + 5));
+        var z = new Big(1/(8*i + 6));
+        pi = pi.plus(sum.times(w.minus(x).minus(y).minus(z)));
+    }
+    console.log(pi.toString());
+    console.log("That took: " + ((new Date().getTime()) - time));
+    */
+    var worker = new Worker("pi_worker.js");
+    
+    worker.onmessage = function(e) {
+        console.log(e.data);
+        console.log("That took: " + ((new Date().getTime()) - time));
+    }
+    var time = new Date().getTime();
+    worker.postMessage("");
+//});
+};
